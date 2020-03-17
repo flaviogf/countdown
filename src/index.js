add0d9396ab1e58415d8947029197ef9a6ad8a76
flaviogf <flavio.fernandes6@gmail.com>
2020-03-16 22:11:33 -0300

    refactor: reseting the project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { addDays, subMilliseconds } from 'date-fns'
-import countdown from './countdown'
-
-function main(start, end) {
-  const result = countdown(start, subMilliseconds(end, 1000))
-  console.log(result)
-}
-
-main(new Date(), addDays(new Date(), 2))
